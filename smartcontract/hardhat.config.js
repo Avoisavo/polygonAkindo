ab7bbe4ad2075d8b4e12ced75107f2803e072a3d
Cedric <cedricctf11a@gmail.com>
2025-11-24 18:51:25 +0800

    added base sepolia

--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -10,6 +10,11 @@ module.exports = {
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
       chainId: 80002,
     },
+    baseSepolia: {
+      url: process.env.BASE_SEPOLIA_RPC_URL || "https://sepolia.base.org",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 84532,
+    },
   },
   etherscan: {
     apiKey: {
